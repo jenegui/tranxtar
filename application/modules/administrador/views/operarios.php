b11cc407c66f0f4087cfb861c520cd708b2d063b
JENEGUI <jenegui@gmail.com>
2019-03-10 17:01:11 -0500

    commit 03102019 2

---
+++ application/modules/administrador/views/operarios.php
@@ -0,0 +1,125 @@
+<!-- -->
+<style>
+	.links strong{
+		color: #F00;
+	}
+	
+	
+	.links a:link {
+		padding: 2px 2px 2px 2px;
+		text-align: center;
+		text-decoration: none;
+		font-size: 12px;
+		color: #000;
+		background: #FFFFFF;
+	}
+	
+	.links a:visited {
+		/*border: 1px solid #F00;*/
+		padding: 2px 2px 2px 2px;
+		text-align: center;
+		text-decoration:none;
+		font-size: 12px; 
+		color:#000;
+	} 
+	
+	.links a:hover {
+		border: 1px solid #000;
+		padding: 2px 2px 2px 2px;
+		text-decoration:underline;
+		font-weight: bolder; 
+		color:#F00; 
+		background: #EEEEEE;
+	}
+</style>
+<?php  //if (($ano_periodo == $reciente["ano"])&&($mes_periodo == $reciente["mes"])){ ?>   
+		  <div class="row">
+	         <div class="fivecol"><h1>Operarios</h1></div>
+	         <div style="text-align: right;" class="sixcol">
+	            <input type="button" id="btnAgregarUsuario" name="btnAgregarUsuario" value="Agregar Usuario" class="button"/>
+                    
+	         </div>
+          </div>	 
+<?php  //} ?>
+
+<table width="100%" style="font-size: 11px;">
+<tr>
+ <td>
+   <div id="divUsuarios">
+   <form id="frmUsuarios" name="frmUsuarios" method="post" action="">
+   <table width="100%" class="table">
+   <thead class="thead">
+   <tr>
+     <th width="40%">Nombre operario</th>
+     <th>Identificaci&oacute;n</th>
+     <th>Tel&eacute;fono</th>
+     <th align="center" width="10%">Modificar</th>
+     <th align="center" width="10%">Inactivar</th>
+     <th align="center" width="10%">Estado</th>
+   </tr>
+   </thead>
+   <tbody>
+   <?php for ($i=0; $i<count($usuarios); $i++){
+    if($usuarios[$i]["nombre"]!=""){   
+   			if (($i%2)==0)
+   				$class = "row1";
+   			else 
+   				$class = "row2";	 
+   ?>
+	   <tr class="<?php echo $class; ?>">
+	     <td>&nbsp;&nbsp;<?php echo strtoupper($usuarios[$i]["nombre"]); ?></td>
+	     <td><?php echo $usuarios[$i]["rol"]; ?></td>
+	     <td align="center">
+	     	 <?php  //if (($ano_periodo == $reciente["ano"])&&($mes_periodo == $reciente["mes"])){ ?> 
+	     	 	<a href="javascript:modificarUsuarioADM(<?php echo $usuarios[$i]["id"]; ?>);"><img src="<?php echo base_url("images/edit.png"); ?>" border="0"/></a>
+	     	 <?php //} ?>
+	     </td>
+	     <td align="center">
+	     	 <?php  //if (($ano_periodo == $reciente["ano"])&&($mes_periodo == $reciente["mes"])){ 
+	     				if ($usuarios[$i]["idxrol"]!=4){ ?>
+	     				<a href="javascript:eliminarUsuarioADM(<?php echo $usuarios[$i]["idxrol"]; ?>,<?php echo $usuarios[$i]["id"]; ?>);"><img src="<?php echo base_url("images/delete.png"); ?>" border="0"/></a>
+	     				<?php } ?>
+	     	  <?php //} ?>	
+	     </td>
+             <td align="center">
+                   <?php echo $usuarios[$i]["estado"]; ?> 
+             </td>
+	   </tr>
+   <?php 
+        }
+    } 
+    ?>
+   </tbody>   
+   <!-- links -->
+   <tfoot>
+	<tr>
+  		<td colspan="7">&nbsp;</td>
+	</tr>
+	<tr>
+		<td colspan="3" align="left">&nbsp;</td>
+		<td colspan="4" align="right" class="links"><?php echo $links; ?></td>
+	</tr>
+	</tfoot>
+   <!-- links -->
+   </table>   
+   </form>
+   </div>   
+ </td> 
+</tr>
+<tr>
+ <td>
+   <div id="detalle"></div>
+ </td> 
+</tr>
+</table>
+<!-- Div para ageragr empresas -->
+<div id="agregarUsuario" style="display: none">
+<?php 
+	$this->load->model("tipodocs");
+        $this->load->model("rol");
+        
+        $data["tipodoc"] = $this->tipodocs->obtenerTipoDocumentos();
+        $data["roles"] = $this->rol->obtenerRoles();
+	$this->load->view("ajxusuarioins",$data); 
+?>
+</div>
---
+++ js/general/ready.js
@@ -0,0 +1,94 @@
+$(function () {
+//Valida el env�o del formulario de agregar empresas
+    $("#btnIngresar").click(function () {
+        $("#frmIngresar").validate({
+            rules: {
+                txtLogin: {required: true},
+                txtPassword: {required: true}
+            },
+            messages: {
+                txtLogin: {required: "Debe ingresar usuario."},
+                txtPassword: {required: "Debe ingresar password."},
+            },
+            errorPlacement: function (error, element) {
+                element.after(error);
+                error.css('opacity','0.47');
+                error.css('z-index','991');
+                error.css('background','#ee0101');
+                //error.css('float','right');
+                error.css('position','absolute');
+                error.css('margin-top','1px');
+                error.css('color','#fff');
+                error.css('font-size','11px');
+                error.css('border','2px solid #ddd');
+                error.css('box-shadow','0 0 6px #000');
+                error.css('-moz-box-shadow','0 0 6px #000');
+                error.css('-webkit-box-shadow','0 0 6px #000');
+                error.css('padding','4px 10px 4px 10px');
+                error.css('border-radius','6px');
+                error.css('-moz-border-radius','6px');
+                error.css('-webkit-border-radius','6px');
+            },
+            submitHandler: function (form) {
+                form.submit();
+                /*$.ajax({
+                    type: "POST",
+                    url: base_url + "login/validar",
+                    data: $("#frmIngresar").serialize(),
+                    dataType: "html",
+                    cache: false,
+                    success: function (data) {
+                        //alert(url);
+                        $("#agregarEmpresa").dialog('close');
+                    }
+                });*/
+            }
+        });
+    });
+});
+
+
+
+
+$("#medio").select2();
+$("#evento").select2();
+$("#anio").select2();
+
+
+
+ $('#fechaIni').datepicker({
+//$('#fechaIni'+ i).datepicker({    
+        dateFormat: 'mm/dd/yy',
+        monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+        'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+        dayNames: ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mie','Jue','Vie','Sab'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa']
+        });
+$('#fechaFin').datepicker({
+        dateFormat: 'mm/dd/yy',
+        monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+        'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+        dayNames: ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mie','Jue','Vie','Sab'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa']
+        });
+
+$(function() {
+    $( "button" )
+    .button()
+    .click(function( event ) {
+        event.preventDefault();
+    });
+});
+
+
+
+//Asociar el widget tabs a la división cuyo id es tabs
+$(function() {
+    $( "#tabs" ).tabs();
+});
+
+
